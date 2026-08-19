1f76551a18a2db2f290611cf8e98e8967ddbecd8
Dennis <dennyfrank320@gmail.com>
2026-08-19 09:39:42 +0200

    perf data for fftw

--- software_profiling/datasets/create_dataset.py
+++ software_profiling/datasets/create_dataset.py
@@ -34,12 +34,12 @@ def create_image_data():
         index+=1
 
 def update_code(index, width, height):
-    with open("edgeBare.c", "r",  encoding='utf-8') as file:
+    with open("fftw.c", "r",  encoding='utf-8') as file:
         data = file.readlines()
         data[0] = "#include \"header/image_data_"+str(index)+".h\"\n"
         data[1] = "#define WIDTH "+ str(width) +"\n"
         data[2] = "#define HEIGHT "+ str(height) + "\n"
-    with open("edgeBare.c", "w",  encoding='utf-8') as file:
+    with open("fftw.c", "w",  encoding='utf-8') as file:
         file.writelines(data)
 
 def collect_data():
@@ -47,8 +47,9 @@ def collect_data():
     for index in range(len(image_size)):
         width, height = image_size[index] #width and height of all images got saved before
         update_code(index+1, width, height) 
-        subprocess.run(["gcc", "-static",  "-o", "edgeBare" , "edgeBare.c" , "-lm"]) #compile the code
-        res = subprocess.run(["perf", "stat" ,"-e" ,"instructions,cycles,branches,cache-references,cache-misses" ,"./edgeBare"], capture_output=True, text=True) #executing perf stat with compiled code
+        subprocess.run(["gcc", "-static",  "-o", "fftw" , "fftw.c" , "-lfftw3", "-lm"]) #compile the code
+        #riscv64-unknown-elf-gcc -static   -I./fftw-3.3.10/install/include   -L./fftw-3.3.10/install/lib -o fftw.elf fftw.c -lfftw3 -lm
+        res = subprocess.run(["perf", "stat" ,"-e" ,"instructions,cycles,branches,cache-references,cache-misses" ,"./fftw"], capture_output=True, text=True) #executing perf stat with compiled code
         #catch relevant values
         ins = re.search("([0-9][0-9.]+)\s*(instructions)", res.stderr)
         cyc = re.search("([0-9][0-9.]+)\s*cycles", res.stderr)
@@ -57,23 +58,17 @@ def collect_data():
         cam = re.search("([0-9][0-9.]+)\s*cache-misses", res.stderr)
 
         if index<1:
-            with open("edge_dec_data.csv", "w") as csv_f:
+            with open("fftw_data.csv", "w") as csv_f:
                 writer = csv.writer(csv_f)
                 writer.writerow(['image_name','instructions','cycles','branches','cache-referencs','cache-misses'])
 
-        with open("edge_dec_data.csv", "a") as csv_f:
+        with open("fftw_data.csv", "a") as csv_f:
             writer = csv.writer(csv_f)
             writer.writerow(["kodim"+str(index+1), ins.group(1), cyc.group(1), bra.group(1), car.group(1), cam.group(1)])
 
 
 
 
-    
-
-
-
-#Ausführung von Befehl
-#subprocess.run(["ls"])
 
 def main():
     create_image_data()
---
+++ software_profiling/datasets/fftw.c
@@ -0,0 +1,49 @@
+#include "header/image_data_24.h"
+#define WIDTH 768
+#define HEIGHT 512
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fftw3.h>
+
+
+// the image array: image_data[HEIGHT][WIDTH]
+int main() {
+    int N = WIDTH * HEIGHT;
+
+    fftw_complex *in  = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+    fftw_complex *out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * N);
+
+    // prepare input
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            int idx = y * WIDTH + x;
+            in[idx][0] = image_data[y][x];
+            in[idx][1] = 0.0;
+        }
+    }
+
+    // perform 2D FFT
+    fftw_plan plan_forward = fftw_plan_dft_2d(HEIGHT, WIDTH, in, out, FFTW_FORWARD, FFTW_ESTIMATE); //plan for complex dft
+    fftw_execute(plan_forward);
+
+    // write result in file 
+    // FILE *fout = fopen("fft_output.txt", "w");
+    // if (!fout) { perror("fft_output.txt"); exit(1); }
+    // for (int i = 0; i < N; i++) {
+    //     fprintf(fout, "%.10f %.10f\n", out[i][0], out[i][1]);
+    // }
+    // fclose(fout);
+
+    //     for (int i = 0; i < N; i++) {
+    //     printf("out[%2d] = %.2f + %.2fi\n", i, out[i][0], out[i][1]);
+    // }
+
+    fftw_destroy_plan(plan_forward);
+    fftw_free(in);
+    fftw_free(out);
+
+    //printf("FFT finished, output in 'fft_output.txt'.\n");
+    return 0;
+}
+

38eb2a8d61d3494c54223c58b993e44615894320
Dennis <dennyfrank320@gmail.com>
2026-08-29 12:38:57 +0200

    new version of create_dataset for edge detection, average values of several runs are created

--- software_profiling/datasets/edge/create_dataset.py
+++ software_profiling/datasets/edge/create_dataset.py
@@ -9,6 +9,7 @@ import numpy as np
 image_size = [] #store size of images for later use
 
 
+
 def create_header(name, index):
     img = Image.open(name).convert("L")
     width, height = img.size # returns #tupel(width, height)
@@ -16,7 +17,7 @@ def create_header(name, index):
     #convert into NumPy-Array 
     pixel_data = np.array(img, dtype=np.uint8) 
     # export it as a C-array
-    with open("header/image_data_"+str(index)+".h", "w") as f:
+    with open("../header/image_data_"+str(index)+".h", "w") as f:
         f.write("#ifndef IMAGE_DATA_H\n#define IMAGE_DATA_H\n\n")
         f.write("#include <stdint.h>\n\n")
         f.write(f"const uint8_t image_data[{height}][{width}] = {{\n") 
@@ -26,24 +27,24 @@ def create_header(name, index):
         f.write("};\n\n#endif\n")
 
 def create_image_data():
-    images = os.listdir("images")
+    images = os.listdir("../images")
     images.sort()
     index = 1
     for im in images:
-        create_header("images/"+im, index)
+        create_header("../images/"+im, index)
         index+=1
 
 def update_code(index, width, height):
     with open("edgeBare.c", "r",  encoding='utf-8') as file:
         data = file.readlines()
-        data[0] = "#include \"header/image_data_"+str(index)+".h\"\n"
+        data[0] = "#include \"../header/image_data_"+str(index)+".h\"\n"
         data[1] = "#define WIDTH "+ str(width) +"\n"
         data[2] = "#define HEIGHT "+ str(height) + "\n"
     with open("edgeBare.c", "w",  encoding='utf-8') as file:
         file.writelines(data)
+    
 
-def collect_data():
-    #index = 0
+def collect_data(run):
     for index in range(len(image_size)):
         width, height = image_size[index] #width and height of all images got saved before
         update_code(index+1, width, height) 
@@ -56,23 +57,30 @@ def collect_data():
         car = re.search("([0-9][0-9.]+)\s*cache-references", res.stderr)
         cam = re.search("([0-9][0-9.]+)\s*cache-misses", res.stderr)
 
-        if index<1:
+        if index<1 and run<1:
             with open("edge_dec_data.csv", "w") as csv_f:
                 writer = csv.writer(csv_f)
-                writer.writerow(['image_name','instructions','cycles','branches','cache-referencs','cache-misses'])
+                writer.writerow(['RUN:' + str(run+1)])
+                writer.writerow(['image_name', 'image_size:(width,height)', 'instructions','cycles','branches','cache-referencs','cache-misses'])
+        elif index<1 and run>=1:
+            with open("edge_dec_data.csv", "a") as csv_f:
+                writer = csv.writer(csv_f)
+                writer.writerow(['RUN:' + str(run+1)])
+                writer.writerow(['image_name', 'image_size:(width,height)', 'instructions','cycles','branches','cache-referencs','cache-misses'])
 
         with open("edge_dec_data.csv", "a") as csv_f:
             writer = csv.writer(csv_f)
-            writer.writerow(["kodim"+str(index+1), ins.group(1), cyc.group(1), bra.group(1), car.group(1), cam.group(1)])
+            writer.writerow(["kodim"+str(index+1), image_size[index], ins.group(1), cyc.group(1), bra.group(1), car.group(1), cam.group(1)])
 
-
+        
+    
 
 
 
 def main():
     create_image_data()
-    collect_data()
-        
+    for run in range(10):
+        collect_data(run)  
 
 
 
